084bbf94582e1b5f76b6906990553b5012f7bb5f
yurket <yurket2006@gmail.com>
2014-12-05 17:24:00 +0300

    moved xml_reader into the header

--- xml_config.cpp
+++ xml_config.cpp
@@ -1,46 +1,10 @@
-#include <iostream>
-
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-
-using namespace std;
-
-
-
-class XmlConfig {
-private:
-    string m_filename;
-
-public:
-    string nick;
-    string real_name;
-    string server_ip;
-    // string server_name;
-
-    XmlConfig(string filename);
-    void load(const string &filename);
-
-};
-
-XmlConfig::XmlConfig(string filename)
-    : m_filename(filename)
-{
-    clog << "CTOR" << endl;
-}
-
-void XmlConfig::load(const string &filename)
-{
-    using boost::property_tree::ptree;
-    ptree pt;
-    read_xml(filename, pt);
-    nick = pt.get("config.nick", "t3st_n1ck");
-    real_name = pt.get("config.real_name", "(empty)");
-
-}
-
+#include "xml_config.hpp"
 
 int main()
 {
     XmlConfig conf("test.xml");
+    conf.load();
+    conf.print_config();
+
     return 0;
 }
---
+++ xml_config.hpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+using namespace std;
+
+
+
+class XmlConfig {
+private:
+    string m_filename;
+
+public:
+    string nick;
+    string real_name;
+    string server_ip;
+    string server_name;
+ 
+    XmlConfig(string filename);
+
+    void load();
+    void load(const string &filename);
+    void print_config();
+
+};
+
+XmlConfig::XmlConfig(string filename)
+    : m_filename(filename)
+{
+    clog << "CTOR" << endl;
+}
+
+void XmlConfig::load()
+{
+    this->load(this->m_filename);
+}
+void XmlConfig::load(const string &filename)
+{
+    using boost::property_tree::ptree;
+    ptree pt;
+    read_xml(filename, pt);
+    nick = pt.get("config.nick", "t3st_n1ck");
+    real_name = pt.get("config.real_name", "(empty)");
+    server_ip = pt.get<string>("config.server_ip");
+    server_name = pt.get<string>("config.server_name");
+}
+
+void XmlConfig::print_config()
+{
+    clog << "\n\nThe CONFIG from the file " << this->m_filename << endl;
+    clog << "nick: " << this->nick << endl;
+    clog << "real name: " << this->real_name << endl;
+    clog << "server_ip: " << this->server_ip << endl;
+    clog << "server_name: " << this->server_name << endl;
+
+}
