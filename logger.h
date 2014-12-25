281dff1847029221f59fb3b6a2d13c63c5c6ac61
yurket <yurket2006@gmail.com>
2014-12-25 22:38:04 +0300

    logger description

---
+++ logger.h
@@ -0,0 +1,23 @@
+#indfef LOGGER_H_
+#define LOGGER_H_
+
+#include <fstream>
+
+
+class Logger
+{
+    std::ofstream file_;
+ public:
+    Logger(const std::string filename, const char mode);
+    void PrintHeader();
+    void Log(const std::string &s);
+    void Log(const char *s);
+};
+
+
+
+Logger::Logger(const std::string filename, const char mode)
+{
+    file_.open(filename, std::ofstream::out | std::ofstream::app);
+}
+#endif // LOGGER_H_
